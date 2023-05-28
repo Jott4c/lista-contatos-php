61543ecabf6e7421a46892acbf5a5739427ec0ce
jott4c <jrcba123@gmail.com>
2023-05-28 05:08:56 -0400

    Initial project structure setup

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jott4c\ContactApi\Controllers;
+
+class UserController
+{
+    public function index()
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function update($id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Configurações de conexão com o banco de dados
+define('DB_HOST', 'localhost');
+define('DB_USER', 'usuario');
+define('DB_PASSWORD', 'senha');
+define('DB_NAME', 'nome_do_banco');
+
+// Chave secreta para autenticação
+define('SECRET_KEY', 'sua_chave_secreta');
